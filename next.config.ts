357b469399c7c314d5de9c5b4a21d3376e790e20
Jose Gambin Manresa <35967457+Josegambin@users.noreply.github.com>
2026-09-10 10:43:11 +0200

    Modify next.config.ts for GitHub Pages support
    Updated next.config.ts to use CommonJS module export and added configuration for GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Indica que exporte a HTML puro en la carpeta 'out'
+  images: {
+    unoptimized: true, // Requerido porque GitHub Pages no soporta la optimización dinámica de imágenes de Next.js
+  },
+  // OPCIONAL: Si tu URL de GitHub Pages incluye el nombre del repositorio (ej. usuario.github.io/mi-repo)
+  // debes agregar la propiedad basePath: '/mi-repo'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
