6e54330669ca3b2966a3c635783bf43ba244bb5c
Jose Gambin <josemanuel.gambin@gmail.com>
2026-09-10 11:19:44 +0200

    no message

--- src/app/sitemap_.ts
+++ src/app/sitemap_.ts
@@ -4,6 +4,7 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://josegambin.dev'
+
   return [
     {
       url: baseUrl,
