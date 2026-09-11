49b472d8d94aa9dc4b2ba72a8ba55aefd1dbffa2
Jose Gambin Manresa <35967457+Josegambin@users.noreply.github.com>
2026-09-11 13:52:47 +0200

    Refactor next.config.ts for GitHub Pages compatibility
    Removed the output property and adjusted the images configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Indica que exporte a HTML puro en la carpeta 'out'
   images: {
-    unoptimized: true, // Requerido porque GitHub Pages no soporta la optimización dinámica de imágenes de Next.js
-  },
-  // OPCIONAL: Si tu URL de GitHub Pages incluye el nombre del repositorio (ej. usuario.github.io/mi-repo)
-  // debes agregar la propiedad basePath: '/mi-repo'
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
